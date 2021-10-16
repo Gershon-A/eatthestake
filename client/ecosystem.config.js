d96c8b5128fbe68edc9491755b4412e61aadc941
Gershon-A <gershon.webgate@gmail.com>
2021-10-16 19:55:57 +0300

    Added fixed docker. Added ecosystem.config.js for process managing
    Signed-off-by: Gershon-A <gershon.webgate@gmail.com>

---
+++ client/ecosystem.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  apps : [{
+    name   : "eatthestake-staging",
+    script : "serve",
+    //args: 'dotenv ../.env.sokol ',
+    watch: true,
+    //error_file: "/dev/stderr",
+    //out_file: "/dev/stdout",
+    env_production: {
+       NODE_ENV: "production",
+       HOST: '0.0.0.0',
+       PM2_SERVE_PATH: 'build',
+       PM2_SERVE_PORT: 4000,
+       PM2_SERVE_HOMEPAGE: '/index.html'
+    },
+    env_development: {
+       NODE_ENV: "development",
+       PM2_SERVE_PATH: 'build',
+       PM2_SERVE_PORT: 4010,
+       PM2_SERVE_HOMEPAGE: '/index.html'
+    },
+    env_staging: {
+      NODE_ENV: "staging",
+    },
+  }] ,
+ /* ToDO */
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
