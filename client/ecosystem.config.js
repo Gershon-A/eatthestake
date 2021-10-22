6fbfbe9fdff626e8e43d2a4ef62d8a41936f698a
Gershon-A <gershon.webgate@gmail.com>
2021-10-22 16:08:50 +0300

    Minor fixex. Sokol deployment test. Node 14.x coverage
    Signed-off-by: Gershon-A <gershon.webgate@gmail.com>

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -2,10 +2,7 @@ module.exports = {
   apps : [{
     name   : "eatthestake",
     script : "serve",
-    //args: 'dotenv ../.env.sokol ',
     watch: true,
-    //error_file: "/dev/stderr",
-    //out_file: "/dev/stdout",
     env_production: {
        NODE_ENV: "production",
        HOST: '0.0.0.0',
@@ -33,6 +30,11 @@ module.exports = {
       path : 'DESTINATION_PATH',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    },
+    development : {
+      'pre-deploy-local': 'yarn build',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env development',
       'pre-setup': ''
     }
   }
